a1faee9a2cf4ac7648730015a241e8dd6c88c19c
Borislav Blagoev <borislav.blagoev@ad.mentormate.bg>
2021-08-13 19:41:15 +0300

    Create NavBar component and add logo and baner

---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>
+      <v-toolbar class="mt-15" color="grey" height="83">
+        <img src="logo.png" />
+        <template>
+          <v-tabs color="white" centered>
+            <v-tabs-slider color="white"></v-tabs-slider>
+            <v-tab v-for="(item, index) in items" :key="index">
+              {{ item.title }}
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "HOME", path: "/" },
+        { title: "TRANSFERS", path: "/transfers" },
+        { title: "CUP", path: "/cup" },
+        { title: "H2H", path: "/h2h" },
+        { title: "RULES & PRIZES", path: "/rules_prizes" },
+        { title: "ADMIN", path: "/admin" },
+        { title: "LOGIN", path: "/login" },
+        { title: "DONATE", path: "/donate" },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
